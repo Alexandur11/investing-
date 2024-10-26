e25d58fcee51db88efebc91ef294371a4576eff6
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-10-26 12:54:07 +0300

    Add automated scraping module

--- app/models/event_handlers/automated_analytics_handlers.py
+++ app/models/event_handlers/automated_analytics_handlers.py
@@ -20,7 +20,7 @@ def stock_collector(url):
 
     if df['next_url']:
         print(df['next_url'])
-        time.sleep(60)
+        time.sleep(20)
         stock_collector(df['next_url'])
     print(f' Stock collector process completed')
 
@@ -29,32 +29,34 @@ def fetch_stocks(limit=1000,url=None):
         api_key = env_vars.get('POLYGONAPIKEY')
         params = {'limit': limit}
         headers = {'Authorization': f'Bearer {api_key}'}
+        print('Data fetched')
         return requests.get(url=url, params=params, headers=headers)
     except Exception as e:
         print(f'Error fetching stocks {e}')
 
 def convert_response_to_dataframe(response):
     try:
+        print(f'Status code {response.status_code}')
         decoded_data = response.content.decode("utf-8")
         json_data = json.loads(decoded_data)
         df = pd.DataFrame(json_data['results'])
         df.dropna()
-
-        return {'df':df['ticker'],'next_url':json_data['next_url']}
+        print(f'Data converted {df}')
+        return {'df':df.loc[df['market'] == 'stocks', 'ticker'],'next_url':json_data['next_url']}
     except Exception as e:
         print(f'Error converting response to dataframe {e}')
 
 def save_dataframe_to_sheets(df):
     try:
         SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-        creds = Credentials.from_service_account_file('stocks-439709-b69ac4781e44.json', scopes=SCOPES)
+        creds = Credentials.from_service_account_file('stocks-439709-358f6c1e35af.json', scopes=SCOPES)
 
         # Authorize the gspread client
         gc = gspread.authorize(creds)
 
         # Use the spreadsheet ID to open the spreadsheet
         spreadsheet_id = '1Hien8Dr0zsu3aorg2-UhuI0w1rmjIdcb5QAqExnyG3Y'
-        worksheet_name = 'Stocks_list'
+        worksheet_name = 'Unfiltered_stocks'
 
         # Open the spreadsheet using its ID
         sh = gc.open_by_key(spreadsheet_id)
@@ -65,6 +67,7 @@ def save_dataframe_to_sheets(df):
 
 
         data_to_append = df.tolist()
+        print(f' Data Size {len(data_to_append)}')
 
         range_to_update = f"{chr(65 + last_column)}1:{chr(65 + last_column)}{len(data_to_append)}"
 
@@ -74,6 +77,8 @@ def save_dataframe_to_sheets(df):
             cell.value = data_to_append[i]
 
         worksheet.update_cells(cell_list)
+
+        print(f'Sheet updated')
     except Exception as e:
         print(f'Error saving dataframe to Google Sheets: {e}')
 
---
+++ app/models/event_handlers/automated_scraping_handlers.py
@@ -0,0 +1,94 @@
+import asyncio
+
+
+async def automated_focus_guru_scrape_orchestrator():
+    filtered_data = []
+
+    unfiltered_data = [None]
+
+    for x in unfiltered_data:
+        scraped_data = None
+
+
+async def data_validation_orchestrator(financial_strengths,
+                                       growth_rank,
+                                       liquidity_raio,
+                                       profitability_rank,
+                                       gf_value_rank):
+    async with asyncio.TaskGroup() as tg:
+        tasks = [
+            tg.create_task(varify_cash_to_debt_ratio(financial_strengths)),
+            tg.create_task(verify_debt_to_equity_ratio(financial_strengths)),
+            tg.create_task(verify_debt_to_ebitda_ratio(financial_strengths)),
+            tg.create_task(verify_interest_coverage_ratio(financial_strengths)),
+            tg.create_task(verify_roe(profitability_rank)),
+            tg.create_task(verify_roa(profitability_rank)),
+            tg.create_task(verify_roic(profitability_rank)),
+            tg.create_task(verify_price_to_earnings_ratio(gf_value_rank, growth_rank)),
+            tg.create_task(verify_peg_ratio(gf_value_rank)),
+            tg.create_task(verify_price_to_sales_ratio(gf_value_rank)),
+            tg.create_task(verify_price_to_book_ratio(gf_value_rank)),
+            tg.create_task(verify_price_to_free_cash_flow_ratio(gf_value_rank))
+        ]
+
+
+async def varify_cash_to_debt_ratio(data):
+    if data['Cash-To-Debt'] > 0.20:
+        return True
+
+async def verify_debt_to_equity_ratio(data):
+    if data['Debt-to-Equity'] < 1:
+        return True
+
+async def verify_debt_to_ebitda_ratio(data):
+    if data['Debt-to-EBITDA'] < 2.5:
+        return True
+
+async def verify_interest_coverage_ratio(data):
+    if data['Interest Coverage'] > 5:
+        return True
+
+async def verify_roe(data):
+    if data['ROE %'] > 12:
+        return True
+
+async def verify_roa(data):
+    if data['ROA %'] > 5:
+        return True
+
+async def verify_roic(data):
+    if data['ROIC %'] > 12:
+        return True
+
+
+async def verify_price_to_earnings_ratio(gf_data,growth_rank_data):
+    revenue = growth_rank_data['3-Year Revenue Growth Rate'] if not None else 0
+    pe = gf_data['PE Ratio']
+
+    if revenue == 0 and pe <11:
+        return True
+
+    if revenue in range(5,8) and pe < 17:
+        return True
+
+    if revenue in range(10,13) and pe <25:
+        return True
+
+
+async def verify_peg_ratio(data):
+    if data['PEG Ratio'] <1:
+        return True
+
+async def verify_price_to_sales_ratio(data):
+    if data['PS Ratio'] < 2:
+        return True
+
+async def verify_price_to_book_ratio(data):
+    if data['PB Ratio'] <3:
+        return True
+
+
+async def verify_price_to_free_cash_flow_ratio(data):
+    if data['Price-to-Free-Cash-Flow'] < 25:
+        return True
+
